68d480a899c345e82ee4e38c59ed691fda7064be
Isaac Afedzi <isaackobinaafedzi@gmail.com>
2025-05-21 10:39:36 +0000

    test case introduced

---
+++ tests/test_operation.py
@@ -0,0 +1,9 @@
+from src.math_operation import addition, subtraction 
+
+def test_add():
+    assert addition(2,3) == 5
+    assert addition(-4, 10) == 6
+    
+def test_minus():
+    assert subtraction(5, 10)==5
+    assert subtraction(-6, 3)==9
